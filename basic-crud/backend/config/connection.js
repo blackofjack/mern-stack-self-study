f993a95c22d7734c1956b81b252d98ea3cab9867
blackofjack <jeraldtamayo.ph@gmail.com>
2026-09-12 13:09:42 +0800

    modify my .gitignore

---
+++ basic-crud/backend/config/connection.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const mongoUri = process.env.MONGODB_URI || process.env.MONGODB_URI;
+    if (!mongoUri) {
+      throw new Error("MONGO_URI is not defined.");
+    }
+
+    const conn = await mongoose.connect(mongoUri);
+
+    console.log(` MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("X MongoDB Connection failed");
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+
+export default connectDB;  //test
