fb6e03ac118cadc8a22219fce09a27d9b5d922fe
blackofjack <jeraldtamayo.ph@gmail.com>
2026-09-12 12:52:57 +0800

    modify env

--- basic-crud/backend/routes/record.js
+++ basic-crud/backend/routes/record.js
@@ -1,2 +1,5 @@
+NODE_ENV=development
 PORT = 5000
-MONGO_URI=mongodb+srv://username:password@cluster.mongodb.net/employees
+MONGO_URI=mongodb+srv://jeraldtamayoph_db_user:ohNDvCQWXv6jViHK@bascimern.sf98fmq.mongodb.net/?appName=bascimern
+
+
